95bd03d60c868618229b70fbe68eee637ae1d5a8
bakroistvan <bakro.istvan@gmail.com>
2026-01-01 14:17:28 +0100

    preset Integrate with the calculated output percent after boost time

--- custom_components/smart_thermostat/climate.py
+++ custom_components/smart_thermostat/climate.py
@@ -1102,6 +1102,10 @@ class SmartThermostat(ClimateEntity, RestoreEntity, ABC):
                     self._pid_controller.mode = "OFF"
                 else:
                     self._pid_controller.mode = "AUTO"
+                    # preset Integrate with the calculated output percent after boost time
+                    time_passed = time.time() - self._time_changed
+                    self._pid_controller.integral = time_passed / self._pwm
+                    self._i = self._pid_controller.integral
 
 
             if self._pid_controller.sampling_period == 0:
