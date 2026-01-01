5dd0e32023ddd227d6400510df135ddd9468d411
bakroistvan <bakro.istvan@gmail.com>
2026-01-01 17:48:00 +0100

    bugfix

--- custom_components/smart_thermostat/climate.py
+++ custom_components/smart_thermostat/climate.py
@@ -726,7 +726,7 @@ class SmartThermostat(ClimateEntity, RestoreEntity, ABC):
         if self._pid_controller:
             self._pid_controller.out_max = self._max_out
             self._pid_controller.out_min = self._min_out
-            await self.clear_integral()
+            self.clear_integral()
         self._time_changed = 0
         self.pi
 
